4ba81ee1dec51b393d338c26c4904d3f2e7c7652
aloksharma29 <54803227+aloksharma29@users.noreply.github.com>
2019-11-17 22:29:38 +0530

    Removed stringasfactors

--- HR Analytics.R
+++ HR Analytics.R
@@ -1,11 +1,10 @@
 setwd("D:/Analyticsvidhya/HR Analytics - Hackathon")
-train_hr <- read.csv("train_LZdllcl.csv", stringsAsFactors = F)
-test_hr <- read.csv("test_2umaH9m.csv", stringsAsFactors = F)
+train_hr <- read.csv("train_LZdllcl.csv")
+test_hr <- read.csv("test_2umaH9m.csv")
 
 View(train_hr)
 View(test_hr)
 
-read.csv()
 
 
 

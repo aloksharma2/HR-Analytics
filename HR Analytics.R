d5d76a1d4d8f3ecd89b595d8213aabe5216aa60f
aloksharma29 <54803227+aloksharma29@users.noreply.github.com>
2019-11-17 19:06:05 +0530

    First Lines
    Note: set working directory as same or change

---
+++ HR Analytics.R
@@ -0,0 +1,11 @@
+setwd("D:/Analyticsvidhya/HR Analytics - Hackathon")
+train_hr <- read.csv("train_LZdllcl.csv", stringsAsFactors = F)
+test_hr <- read.csv("test_2umaH9m.csv", stringsAsFactors = F)
+
+View(train_hr)
+View(test_hr)
+
+read.csv()
+
+
+

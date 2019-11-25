218282a0379637c07643ca33d828b10343ddbc64
aloksharma29 <54803227+aloksharma29@users.noreply.github.com>
2019-11-25 19:50:27 +0530

    new

--- HR Analytics.R
+++ HR Analytics.R
@@ -1,10 +1,63 @@
 setwd("D:/Analyticsvidhya/HR Analytics - Hackathon")
+require(dplyr)
+require(ggplot2)
+
+#Loading both the data sets
 train_hr <- read.csv("train_LZdllcl.csv")
 test_hr <- read.csv("test_2umaH9m.csv")
 
-View(train_hr)
-View(test_hr)
+#merging the data sets to perform EDA at the same time
+master_data <- bind_rows(train_hr, test_hr)
+View(master_data)
 
-master_data <- rbindtrain_hr, test_hr)
+str(master_data)
+summary(master_data)
+
+master_data <- mutate_if(master_data, is.character, toupper)
+
+#checking spaces and blanks
+colSums(master_data == "" | master_data == " ", na.rm = T)
+
+table(master_data$education)
+
+ggplot(train_hr, aes(x=education, 
+                     fill=as.factor(is_promoted))) + geom_bar(position = "fill")
+
+
+master_data[which(master_data$education == "  ")] <- "primary"
+
+View(master_data)
+hh
+
+
+
+
+
+
+
+#checking NA values
+colSums(is.na(master_data))
+
+
+ggplot(master_data, aes(x=previous_year_rating, 
+                        fill=as.factor(is_promoted))) + geom_bar(position = "fill")
+
+#creates a dataset containing NA values in previous year data
+pyr_missing <- filter(master_data, is.na(master_data$previous_year_rating))
+View(pyr_missing)
+
+require(mice)
+pyr_missing1 <- mice(master_data, is.na(master_data$previous_year_rating))
+
+
+
+
+
+
+
+
+
+
+
 
 
